5c09f1b6d45b82af38bbea20f77f91830a74a090
Treasure Uzoma <treasureuzoma650@gmail.com>
2024-09-30 00:39:50 +0100

    probably fixed simething

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,7 @@
-import type { Metadata } from "next";
 import "./globals.css";
 
 import Nav from "./components/Nav";
 import Hero from "./components/Hero";
-
-export const metadata: Metadata = {
-    title: "Treasure Uzoma - Portfolio",
-    description:
-        "3+ years of experience. Specialized in building products, while ensuring a seamless web experience for clients across Africa.",
-    themeColor: "#061417"
-};
 
 function Home() {
     return (
