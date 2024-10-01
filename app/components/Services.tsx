013f509583743f56e031c4cf876287531e464413
Treasure Uzoma <123127790+TreasureUzoma@users.noreply.github.com>
2024-10-01 11:56:53 +0100

    Update Services.tsx

--- app/components/Services.tsx
+++ app/components/Services.tsx
@@ -100,9 +100,9 @@ const Services = () => {
                         ].map((service, index) => (
                             <motion.div
                                 key={index}
-                                ref={el => {
-                                    refs.current[index] = el;
-                                }} // Assign each block's ref without returning
+                               ref={(el) => refs.current[index] = el as HTMLDivElement | null;
+}}
+                                // Assign each block's ref without returning
                                 className={`w-full h-[270px] flex items-center justify-center relative overflow-hidden px-7 bg-[#0a1c20] ${service.className}`}
                                 variants={itemVariants}
                                 initial="hidden"
