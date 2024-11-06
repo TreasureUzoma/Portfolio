f825dbbbdcc6f3d6d5ccd41cde9b516b4d957f27
treasure <treasureuzoma650@gmail.com>
2024-11-06 04:35:06 -0800

    fixes

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,70 +1,75 @@
 const Footer = () => {
     const iconStyle = "rounded-full border border-1 border-[#0a1c20] px-4 p-3 text-sm bg-[#0a1c20] text-[#34afcb] hover:border-[#34afcb] hover:bg-[#34afcb] hover:text-[#0a1c20]";
     return (
-        <footer>
-            <div className="flex justify-center py-3 font-rubik">
-                <div className="my_fixed_width text-center py-5">
-                    <h2 className="font-bold text-2xl font-clash">
-                        <span>Start a project</span>
-                        <span className="animate-bounce inline-block">🚀</span>
-                    </h2>
-                    <p className="text-[#808e91] font-rubik text-sm my-2">
-                        Interested in working together? We should queue up a
-                        time to chat. I’ll buy the coffee :)
-                    </p>
-                    <hr className="my-[2.7rem] border-[#4c5a5d] inline-block w-[65%] md:w-[45%]" />
-                    <div className=" flex justify-center items-center flex-col md:space-x-4 md:flex-row md:flex-wrap">
-                        <div className="flex justify-center items-center space-x-4 mb-4 md:mb-0">
-                            <a
-                                href="https://www.facebook.com/profile.php?id=100085862181551"
-                                className={iconStyle}
-                            >
-                                <i className="fab fa-facebook"></i>
-                            </a>
-                            <a
-                                href="https://twitter.com/TreasureUzoma1"
-                                className={iconStyle}
-                            >
-                                <i className="fab fa-twitter"></i>
-                            </a>
-                            <a
-                                href="https://www.instagram.com/naira_jasper"
-                                className={iconStyle}
-                            >
-                                <i className="fab fa-instagram"></i>
-                            </a>
-                        </div>
-                        <div className="flex justify-center items-center space-x-4">
-                            <a
-                                href="https://www.github.com/TreasureUzoma"
-                                className={iconStyle}
-                            >
-                                <i className="fab fa-github"></i>
-                            </a>
-                            <a
-                                href="https://wa.me/+2349133354993"
-                                className={iconStyle}
-                            >
-                                <i className="fab fa-whatsapp"></i>
-                            </a>
-                            <a
-                                href="mailto: treasureuzoma650@gmail.com"
-                                className={iconStyle}
-                            >
-                                <i className="fa fa-envelope"></i>
-                            </a>
-                        </div>
-                    </div>
-                    <div className="text-white font-bold text-sm mt-[5rem] my-[2rem]">
-                        <p>&copy; twentytwentyfour</p>
-                        <p className="mt-4">Treasure Uzoma</p>
-                    </div>
-                    <small className="py-3">Design inspired by <a href="https://vickyjay.vercel.app" target="_blank" className="font-bold"
-                        > VickyJay</a>
-                    </small>
-                </div>
+      <footer>
+        <div className="flex justify-center py-3 font-rubik">
+          <div className="my_fixed_width text-center py-5">
+            <h2 className="font-bold text-2xl font-clash">
+              <span>Start a project</span>
+              <span className="animate-bounce inline-block">🚀</span>
+            </h2>
+            <p className="text-[#808e91] font-rubik text-sm my-2">
+              Interested in working together? We should queue up a time to chat.
+              I’ll buy the coffee :)
+            </p>
+            <hr className="my-[2.7rem] border-[#4c5a5d] inline-block w-[65%] md:w-[45%]" />
+            <div className=" flex justify-center items-center flex-col md:space-x-4 md:flex-row md:flex-wrap">
+              <div className="flex justify-center items-center space-x-4 mb-4 md:mb-0">
+                <a
+                  href="https://www.facebook.com/profile.php?id=100085862181551"
+                  className={iconStyle}
+                >
+                  <i className="fab fa-facebook"></i>
+                </a>
+                <a
+                  href="https://twitter.com/TreasureUzoma1"
+                  className={iconStyle}
+                >
+                  <i className="fab fa-twitter"></i>
+                </a>
+                <a
+                  href="https://www.instagram.com/naira_jasper"
+                  className={iconStyle}
+                >
+                  <i className="fab fa-instagram"></i>
+                </a>
+              </div>
+              <div className="flex justify-center items-center space-x-4">
+                <a
+                  href="https://www.github.com/TreasureUzoma"
+                  className={iconStyle}
+                >
+                  <i className="fab fa-github"></i>
+                </a>
+                <a href="https://wa.me/+2349133354993" className={iconStyle}>
+                  <i className="fab fa-whatsapp"></i>
+                </a>
+                <a
+                  href="mailto: treasureuzoma650@gmail.com"
+                  className={iconStyle}
+                >
+                  <i className="fa fa-envelope"></i>
+                </a>
+              </div>
             </div>
-        </footer>
+            <div className="text-white font-bold text-sm mt-[5rem] my-[2rem]">
+              <p>&copy; twentytwentyfour</p>
+              <p className="mt-4">Treasure Uzoma</p>
+            </div>
+            <small className="py-3">
+              Design inspired by{" "}
+              <a
+                href="https://vickyjay.vercel.app"
+                target="_blank"
+                className="font-bold"
+              >
+                {" "}
+                VickyJay
+              </a>
+            </small>
+          </div>
+        </div>
+      </footer>
     );
 };
 export default Footer;
--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -7,72 +7,74 @@ import Image from "next/image";
 
 const Hero = () => {
     return (
-        <header className="my-[5.4rem] flex items-center justify-center min-h-screen relative overflow-hidden w-full md:min-h-[55vh] lg:min-h-[100vh] md:mt-0">
-            <div className="my_fixed_width md:flex justify-between items-center sm:pr-2 md:px-3">
-                <div className="md:w-[52%] lg:w-[60%]">
-                    <h1 className="gradient_text text-[2.1rem] font-clash leading-[2.5rem] md:text-[2.83rem]">
-                        Creative Frontend Developer.
-                    </h1>
-                    <div className="font-[400] mt-[2.5rem] font-rubik pr-5 text-sm">
-                        <p className="gradient_text">
-                            I code and design simple beautiful things, and I
-                            love what I do.
-                        </p>
-                        <p className="gradient_text py-[1.8rem]">
-                            3+ years of experience. Specialized in building
-                            products, while ensuring a seamless web experience
-                            for clients across Africa.
-                        </p>
-                        <p className="gradient_text">
-                            Highly skilled at progressive enhancement, design
-                            systems & UI Engineering - performance and
-                            scalability are top objectives on my radar.
-                        </p>
-                    </div>
-                    <div className="mt-[2rem] flex space-x-5">
-                        <a
-                            href="https://www.facebook.com/profile.php?id=100085862181551"
-                            className="p-1 rounded-full border border-2 border-[#34afcb]"
-                            target="_blank"
-                        >
-                            <FacebookIcon />
-                        </a>
-                        <a
-                            href="https://www.linkedin.com/in/treasure-uzoma-40a789260"
-                            className="p-1 rounded-full border border-2 border-[#34afcb]"
-                            target="_blank"
-                        >
-                            <LinkedinIcon/>
-                        </a>
-                        <a
-                            href="https://www.twitter.com/TreasureUzoma1"
-                            className="p-1 rounded-full border border-2 border-[#34afcb]"
-                            target="_blank"
-                        >
-                            <TwitterIcon />
-                        </a>
-                        <a
-                            href="https://www.instagram.com/naira_jasper"
-                            className="p-1 rounded-full border border-2 border-[#34afcb]"
-                            target="_blank"
-                        >
-                            <InstagramIcon />
-                        </a>
-                    </div>
-                </div>
-                <div className="flex justify-center">
-                    <div className="avatar_container inline-block">
-                        <Image
-                            src="/images/hero.png"
-                            width={280}
-                            height={280}
-                            alt="avatar"
-                            className="avatar_pic block"
-                        />
-                    </div>
-                </div>
+      <header className="my-[5.4rem] flex items-center justify-center min-h-screen relative overflow-hidden w-full lg:min-h-[100vh] md:mt-0">
+        <div className="my_fixed_width md:flex justify-between items-center sm:pr-2 md:px-3">
+          <div className="md:w-[52%] lg:w-[60%]">
+            <h1 className="gradient_text text-[2.1rem] font-clash leading-[2.5rem] md:text-[2.83rem]">
+              Creative Frontend Developer.
+            </h1>
+            <div className="font-[400] mt-[2.5rem] font-rubik pr-5 text-sm">
+              <p className="gradient_text">
+                Crafting simple, beautiful, and intuitive digital experiences is
+                what I do best.
+              </p>
+              <p className="gradient_text py-[1.8rem]">
+                With over 3 years of experience, I specialize in building
+                powerful products that create seamless, enjoyable web
+                experiences for clients across Africa and beyond.
+              </p>
+              <p className="gradient_text">
+                I’m dedicated to progressive enhancement, design systems, and UI
+                engineering, with a constant focus on performance and
+                scalability to bring ideas to life.
+                <br />
+                Let’s make something amazing together.
+              </p>
             </div>
-        </header>
+            <div className="mt-[2rem] flex space-x-5">
+              <a
+                href="https://www.facebook.com/profile.php?id=100085862181551"
+                className="p-1 rounded-full border border-2 border-[#34afcb]"
+                target="_blank"
+              >
+                <FacebookIcon />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/treasure-uzoma-40a789260"
+                className="p-1 rounded-full border border-2 border-[#34afcb]"
+                target="_blank"
+              >
+                <LinkedinIcon />
+              </a>
+              <a
+                href="https://www.twitter.com/TreasureUzoma1"
+                className="p-1 rounded-full border border-2 border-[#34afcb]"
+                target="_blank"
+              >
+                <TwitterIcon />
+              </a>
+              <a
+                href="https://www.instagram.com/naira_jasper"
+                className="p-1 rounded-full border border-2 border-[#34afcb]"
+                target="_blank"
+              >
+                <InstagramIcon />
+              </a>
+            </div>
+          </div>
+          <div className="flex justify-center">
+            <div className="avatar_container inline-block">
+              <Image
+                src="/images/hero.png"
+                width={280}
+                height={280}
+                alt="avatar"
+                className="avatar_pic block"
+              />
+            </div>
+          </div>
+        </div>
+      </header>
     );
 };
 export default Hero;
